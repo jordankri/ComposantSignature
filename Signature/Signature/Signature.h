483a58ca17cfbfb8ba4885aa6f33c4ac64c3c595
Mathieu Louis <louima@users.noreply.github.com>
2017-03-01 23:28:57 +0100

    Spec Signature (#1)

---
+++ Signature/Signature/Signature.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef Signature_h
+#define Signature_h
+
+#include <vector>
+
+/**
+* Génere la signature relative à une transaction et une clé privé afin depouvoir authentifier la transacrtion effectué par
+* le detenteur de la clé privé.
+* data représente un array d'octets à signer et privKey la clée privée.
+*/
+std::string sign(const std::vector<std::uint8_t>& data, std::string privKey);
+
+
+/**
+* Permet de vérifier la véracité de la signature.
+* Grace à la clé publique, on pourra s'assurer que la transaction effectué est bien valide et à été émise par telle
+* personne.
+* data représente l'array d'octets associés à la signature et pubKey la clée publique du signataire.
+*/
+bool verify(const std::vector<std::uint8_t>& data, std::string signature, std::string pubKey);
+
+
+#endif
